4a2c017094ec3b1a0d0337b35183e595e5d363df
Rohan Kalgutkar <rohankalgutkar@gmail.com>
2024-02-15 14:58:13 +0530

    Updated github action

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://rohankalgutkar.github.io/rohankalgutkar-react",
   plugins: [react()],
   resolve: {
     alias: {

8040dc42a217ad1c3dcbba8cfe5ce3e768c8506d
Rohan Kalgutkar <rohankalgutkar@gmail.com>
2024-03-20 21:14:36 +0530

    Added more metrics

--- src/components/tag.js
+++ src/components/tag.js
@@ -1,12 +1,13 @@
 export default function tag() {
   const origin = window.origin;
   const referrer = document.referrer;
+  const timestamp = Date.now();
 
   fetch("https://poc-analytics-production.up.railway.app/tag", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ origin, referrer }),
+    body: JSON.stringify({ origin, timestamp, referrer }),
   });
 }
